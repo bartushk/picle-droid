25c7b11397881baf5eebd01ef1824532b8bb4163
Kyle Bartush <bartushk@gmail.com>
2016-05-12 00:42:23 -0400

    First test of opencv and picle on android.

--- app/src/main/java/net/android/bartushk/picle/MainActivity.java
+++ app/src/main/java/net/android/bartushk/picle/MainActivity.java
@@ -3,11 +3,24 @@ package net.android.bartushk.picle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import net.bartushk.picle.Filter.*;
+
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("opencv_java3");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String path = net.bartushk.picle.Program.class.getResource("/images/cat1.jpeg").getPath();
+        IFilter filter = new ColorFilter();
+        Mat img = Imgcodecs.imread(path);
+        Mat filtered = filter.applyFilter(img);
     }
 }
